1402ab6e3cf364f25b2b3349b69934909a117b2b
95tuanle <tlna95@icloud.com>
2024-01-30 12:04:41 -0500

    add swagger

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,19 @@ import express from 'express'
 import helmet from 'helmet'
 import cors from 'cors'
 import logger from 'morgan'
+import swaggerUi from 'swagger-ui-express'
+import swaggerJsdoc from 'swagger-jsdoc'
 import irisRouter from './routers/iris-router'
+
+const specs = swaggerJsdoc({
+  definition: {
+    openapi: '3.1.0', info: {
+      title: 'Iris API', version: '1.0.0', description: 'An API for Iris data',
+    }, servers: [{
+      url: 'http://localhost:3000',
+    }],
+  }, apis: ['./src/routers/*.ts'],
+})
 
 const app = express()
 app.use(helmet())
@@ -10,12 +22,7 @@ app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 
-app.get('/', (req, res) =>
-  res.send(
-    'Iris API is running! 🌺 🌼 🌸 🌻 🌹 🌷 🌱 🌲 🌳 🌴 🌵 🌾 🌿 🍀 🍁 🍂 🍃 🍄 🌰',
-  ),
-)
-
+app.use('/', swaggerUi.serve, swaggerUi.setup(specs))
 app.use('/api/v1/iris', irisRouter)
 
 app.use((req, res) => res.status(404).send('Not found'))
--- src/routers/iris-router.ts
+++ src/routers/iris-router.ts
@@ -9,10 +9,89 @@ import {
 
 const router = Router()
 
+/**
+ * @swagger
+ * /api/v1/iris:
+ *  post:
+ *    summary: Create a new Iris data entry
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *        schema:
+ *          $ref: '#/components/schemas/Iris'
+ *    responses:
+ *      200:
+ *        description: The created Iris data entry
+ */
 router.post('/', createIrisData)
+
+/**
+ * @swagger
+ * /api/v1/iris:
+ *  get:
+ *    summary: Get all Iris data entries
+ *    responses:
+ *      200:
+ *        description: A list of Iris data entries
+ */
 router.get('/', getAllIrisData)
+
+/**
+ * @swagger
+ * /api/v1/iris/{_id}:
+ *  get:
+ *    summary: Get an Iris data entry by its ID
+ *    parameters:
+ *      - in: path
+ *        name: _id
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      200:
+ *        description: An Iris data entry
+ */
 router.get('/:_id', getIrisDataById)
+
+/**
+ * @swagger
+ * /api/v1/iris/{_id}:
+ *  patch:
+ *    summary: Update an Iris data entry by its ID
+ *    parameters:
+ *      - in: path
+ *        name: _id
+ *        required: true
+ *        schema:
+ *          type: string
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *        schema:
+ *          $ref: '#/components/schemas/Iris'
+ *    responses:
+ *      200:
+ *        description: The updated Iris data entry
+ */
 router.patch('/:_id', updateIrisData)
+
+/**
+ * @swagger
+ * /api/v1/iris/{_id}:
+ *  delete:
+ *    summary: Delete an Iris data entry by its ID
+ *    parameters:
+ *      - in: path
+ *        name: _id
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      200:
+ *        description: The deleted Iris data entry
+ */
 router.delete('/:_id', deleteIrisData)
 
 export default router
