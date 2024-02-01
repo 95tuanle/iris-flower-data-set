e001120c95fd41792d342f146d37ce0ae65d33ff
95tuanle <tlna95@icloud.com>
2024-02-01 11:34:24 -0500

    hello

--- src/app.ts
+++ src/app.ts
@@ -8,14 +8,22 @@ import irisRouter from './routers/iris-router'
 
 const specs = swaggerJsdoc({
   definition: {
-    openapi: '3.1.0', info: {
-      title: 'Iris API', version: '1.0.0', description: 'An API for Iris data',
-    }, servers: [{
-      url: 'http://localhost:3000',
-    }, {
-      url: 'https://iris-flower-data-set.onrender.com',
-    }],
-  }, apis: ['./src/routers/*.ts'],
+    openapi: '3.1.0',
+    info: {
+      title: 'Iris API',
+      version: '1.0.0',
+      description: 'An API for Iris data',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+      {
+        url: 'https://iris-flower-data-set.onrender.com',
+      },
+    ],
+  },
+  apis: ['./src/routers/*.ts'],
 })
 
 const app = express()
