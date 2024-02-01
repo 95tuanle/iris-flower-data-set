002f7a995e8bba4ff5f316e171d912933451daf2
95tuanle <tlna95@icloud.com>
2024-02-01 11:29:50 -0500

    test again

--- src/app.ts
+++ src/app.ts
@@ -8,19 +8,12 @@ import irisRouter from './routers/iris-router'
 
 const specs = swaggerJsdoc({
   definition: {
-    openapi: '3.1.0',
-    info: {
-      title: 'Iris API',
-      version: '1.0.0',
-      description: 'An API for Iris data',
-    },
-    servers: [
-      {
-        url: 'http://localhost:3000',
-      },
-    ],
-  },
-  apis: ['./src/routers/*.ts'],
+    openapi: '3.1.0', info: {
+      title: 'Iris API', version: '1.0.0', description: 'An API for Iris data',
+    }, servers: [{
+      url: 'http://localhost:3000',
+    }],
+  }, apis: ['./src/routers/*.ts'],
 })
 
 const app = express()
@@ -28,7 +21,7 @@ app.use(helmet())
 app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
-app.use('/', (req, res) => res.send('Welcome to Iris API! Please go to /docs for the documentation'))
+app.get('/', (req, res) => res.send('Iris API <a href="/docs">Docs</a'))
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs))
 app.use('/api/v1/iris', irisRouter)
 
